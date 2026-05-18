145ba9419e410d2b59c274b587cc5914e5fb1d4b
Nguyễn Sơn Tùng <tungns@mor.com.vn>
2026-05-18 15:48:12 +0700

    Update phía web chạy đc

--- demo-aspire-2-main/AspireApp.Web/WeatherApiClient.cs
+++ demo-aspire-2-main/AspireApp.Web/WeatherApiClient.cs
@@ -6,7 +6,7 @@ public class WeatherApiClient(HttpClient httpClient)
     {
         List<WeatherForecast>? forecasts = null;
 
-        await foreach (var forecast in httpClient.GetFromJsonAsAsyncEnumerable<WeatherForecast>("/weatherforecast", cancellationToken))
+        await foreach (var forecast in httpClient.GetFromJsonAsAsyncEnumerable<WeatherForecast>("/weatherforecasts", cancellationToken))
         {
             if (forecasts?.Count >= maxItems)
             {
